c66ff67169e405c601be2d6325eaeade46de3896
Dylan Kershaw <dylankershaw@me.com>
2018-12-27 17:07:00 -0500

    build Intent, Phrase, and User models

--- api/models.py
+++ api/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+
+class Intent(models.Model):
+    name = models.CharField(max_length=255)
+
+    # TODO: use a Signal with the intent's name so it can be called with a generic Intent.trigger method
+    # https://docs.djangoproject.com/en/2.1/topics/signals/
+
+    def __str__(self):
+        return self.name
+
+class Phrase(models.Model):
+  name = models.CharField(max_length=255)
+  intent = models.ForeignKey(Intent, on_delete=models.CASCADE)
+
+  def __str__(self):
+        return self.name
+
+class User(models.Model):
+    name = models.CharField(max_length=255)
+    sentiment_score = models.FloatField()
+
+    def __str__(self):
+        return self.name
