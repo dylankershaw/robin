ce69d4efc2eb4d0c87ed4a99d54c42318bbe7360
github username <dylankershaw@me.com>
2019-01-06 14:41:12 -0500

    working /query endpoint

--- api/models.py
+++ api/models.py
@@ -1,22 +1,24 @@
 from django.db import models
 
 
-
 class Intent(models.Model):
     name = models.CharField(max_length=255)
 
-    # TODO: use a Signal with the intent's name so it can be called with a generic Intent.trigger method
+    # TODO: use a Signal with the intent's name so it can be called with a generic Intent.trigger method?
     # https://docs.djangoproject.com/en/2.1/topics/signals/
+    # or should intent-specific methods be called directly from the Query.post view?
 
     def __str__(self):
         return self.name
 
-class Phrase(models.Model):
-  name = models.CharField(max_length=255)
-  intent = models.ForeignKey(Intent, on_delete=models.CASCADE)
 
-  def __str__(self):
+class Phrase(models.Model):
+    name = models.CharField(max_length=255)
+    intent = models.ForeignKey(Intent, on_delete=models.CASCADE)
+
+    def __str__(self):
         return self.name
+
 
 class User(models.Model):
     name = models.CharField(max_length=255)
