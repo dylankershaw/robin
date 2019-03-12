8c71be4240a93cc713868d7bac7a58c1a16dd611
github username <dylankershaw@me.com>
2019-03-11 21:19:45 -0400

    add get_time

--- api/intent_methods.py
+++ api/intent_methods.py
@@ -1,3 +1,11 @@
+import datetime
+
+
 class IntentMethods:
     def greeting():
         return 'Well hello there.'
+
+    def get_time():
+        time = datetime.datetime.now()
+        minutes = "o clock" if time.minute == 0 else time.minute
+        return f'The time is {time.hour} {minutes}'
