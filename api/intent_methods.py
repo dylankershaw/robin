67763a536cf43378ee9e1665355119a543bf84e4
Dylan Kershaw <dylankershaw@me.com>
2019-03-19 16:59:45 -0400

    add error handling

--- api/intent_methods.py
+++ api/intent_methods.py
@@ -9,3 +9,6 @@ class IntentMethods:
         time = datetime.datetime.now()
         minutes = "o clock" if time.minute == 0 else time.minute
         return f'The time is {time.hour} {minutes}'
+
+    def intent_not_found():
+        return 'I don\'t understand'
