f3d67c4d022e97ae1e3bcd9e2185eb3d0f3c70dd
dylankershaw <dylankershaw@me.com>
2019-09-18 01:10:13 -0400

    create placeholder chat page

--- robin/urls.py
+++ robin/urls.py
@@ -14,10 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.conf.urls import url
+from django.conf.urls import include, url
 from api.views import respond
+from robin.views import chat_page
 
 urlpatterns = [
     url('admin/', admin.site.urls),
     url(r'^query/$', respond),
+    url('chat/', chat_page),
 ]
